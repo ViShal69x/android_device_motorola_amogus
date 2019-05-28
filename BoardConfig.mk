2e3c3d23e44fa75415fbe527504276ef8c3203dc
Felix <google@ix5.org>
2019-05-28 13:42:39 +0200

    rav: Makefile: Use BOARD_VENDOR_SEPOLICY_DIRS
    BOARD_SEPOLICY_DIRS is deprecated
    
    Change-Id: Ia66f8462d7b7c546d8bd4d25c6fdfd22b1726841
    Signed-off-by: Felix <google@ix5.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -298,7 +298,7 @@ VENDOR_SECURITY_PATCH := 2020-03-01
 
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
 # Timeservice
