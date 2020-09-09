8c12f7032de9797ca9032ead5b81b6f7c9bce239
Lucchetto <lucchetto.tie@live.com>
2020-09-09 23:56:02 +0200

    rav: build vendor libhwbinder
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Change-Id: Ib20e2672754490fe87d6dcad7b682d7efd70d7e2

--- device.mk
+++ device.mk
@@ -232,7 +232,8 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
-    android.hidl.manager@1.0-java
+    android.hidl.manager@1.0-java \
+    libhwbinder.vendor
 
 # IMS
 PRODUCT_PACKAGES += \
