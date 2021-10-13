9e8029b0598fab014f405eddaae85e14d27ba516
electimon <electimon@gmail.com>
2021-10-13 08:29:18 +0000

    Revert "[TEMP] Disable ravFrameworkOverlay"
    This reverts commit 4c817953631bfd58276f05d658f8757128beb950.

--- device.mk
+++ device.mk
@@ -52,6 +52,7 @@ PRODUCT_VENDOR_KERNEL_HEADERS := device/motorola/amogus-kernel/trinket/kernel-he
 
 # Overlays
 PRODUCT_PACKAGES += \
+    ravFrameworkOverlay \
     sofiaFrameworkOverlay \
     sofiapFrameworkOverlay \
     sofiapSFrameworkOverlay \
